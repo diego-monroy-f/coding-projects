585099bcb33550056395ab9a3ee3841d243923e2
Diego Monroy F <diego.monroy.f@gmail.com>
2024-03-20 09:28:21 -0500

    Using functional components.

---
+++ go-react-app/go-react/src/AppFunction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AppFunction() {
+    return (
+        <>
+            <hr></hr>
+            <h1>Hello Function World!</h1>
+        </>
+    )
+}
+
+export default AppFunction;
--- go-react-app/go-react/src/index.js
+++ go-react-app/go-react/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppClass from './AppClass';
+import AppFunction from './AppFunction';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppClass />
+    <AppFunction />
   </React.StrictMode>
 );
