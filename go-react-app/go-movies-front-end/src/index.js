400c2d79b7a9a59cd500900da2d4dede297864a5
Diego Monroy F <diego.monroy.f@gmail.com>
2024-05-06 18:30:40 -0500

    Add React Router.

--- go-react-app/go-movies-front-end/src/index.js
+++ go-react-app/go-movies-front-end/src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+import Home from "./components/Home";
+import Movies from "./components/Movies";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home/>},
+      {path: "/movies", element: <Movies/>},
+    ]
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
